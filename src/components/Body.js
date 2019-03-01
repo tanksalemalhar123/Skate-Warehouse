013b6021cc5bdd829a5def28abc9687c04b09ab9
tanksalemalhar123 <tanksalemalhar123@gmail.com>
2019-03-01 12:57:21 +0530

    First Version

---
+++ src/components/Body.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react';
+import Image from './rs.png';
+import Image2 from './ns.png';
+
+
+class Body extends Component {
+    render() {
+        return (
+            <div className="col-lg-12 col-md-12 col-sm-12">
+                    <div className="col-lg-3 col-md-3 col-sm-12" style={{paddingTop:'2em'}}>
+                    <div className="sidenav">
+                        <a href="#about" style={{borderLeft:'5px solid #F6510E'}}>SKATEBOARD</a>
+                        <a href="#services">LONGBOARD</a>
+                        <a href="#clients">ACCESSORIES</a>
+                        <a href="#contact">BRANDS</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-9 col-md-9 col-sm-12" style={{paddingTop:'2em'}}>
+                        <div className="col-3 col-md-3 col-sm-3" style={{padding:'1em',cursor:'pointer'}}>
+                           
+                        <div class="card" >
+                               <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                             
+                               <img class="activator" src={Image2} style={{width:'8em'}} />
+                               </div>
+                               
+                               <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                              
+                                    <center>
+                                    <h4 style={{width:'8em'}}>Wonder Women</h4>    
+                                    <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                    <br/>
+                                    <h3 style={{width:'auto',color:'red'}}> $74.99</h3>
+                                    </center>
+                                </div>
+                           </div>              
+                                 </div>  
+
+
+                              <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                              <div class="card" >
+                               <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                               <img class="activator" src={Image} style={{width:'8em'}} />
+                               </div>
+                               
+                               <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                              
+                                    <center>
+                                    <h4 style={{width:'8em'}}>Bling Mate OG</h4>    
+                                    <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                    <br/>
+                                    <h3 style={{width:'auto',color:'red'}}> $84.99</h3>
+                                    </center>
+                                </div>
+                           </div>              
+                                 </div>  
+
+                                 <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                              <div class="card" >
+                               <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                               <img class="activator" src={Image2} style={{width:'8em'}} />
+                               </div>
+                               
+                               <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                              
+                                    <center>
+                                    <h4 style={{width:'8em'}}>Wonder Women</h4>      
+                                    <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                    <br/>
+                                    <h3 style={{width:'auto',color:'red'}}> $74.99</h3>
+                                    </center>
+                                </div>
+                           </div>              
+                                 </div>  
+
+                                 <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                              <div class="card" >
+                               <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                               <img class="activator" src={Image} style={{width:'8em'}} />
+                               </div>
+                               
+                               <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                              
+                                    <center>
+                                    <h4 style={{width:'8em'}}>Bling Mate OG</h4>  
+                                    <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                    <br/>
+                                    <h3 style={{width:'auto',color:'red'}}> $84.99</h3>
+                                    </center>
+                                </div>
+                           </div>              
+                                 </div>  
+
+                                 <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                              <div class="card" >
+                               <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                               <img class="activator" src={Image2} style={{width:'8em'}} />
+                               </div>
+                               
+                               <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                              
+                                    <center>
+                                    <h4 style={{width:'8em'}}>Wonder Women</h4>   
+                                    <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                    <br/>
+                                    <h3 style={{width:'auto',color:'red'}}> $74.99</h3>
+                                    </center>
+                                </div>
+                           </div>              
+                                 </div>  
+
+                                 <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                           <div class="card" >
+                            <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                            <img class="activator" src={Image} style={{width:'8em'}} />
+                            </div>
+                            
+                            <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                           
+                                 <center>
+                                 <h4 style={{width:'8em'}}>Bling Mate OG</h4>      
+                                 <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                 <br/>
+                                 <h3 style={{width:'auto',color:'red'}}> $84.99</h3>
+                                 </center>
+                             </div>
+                        </div>              
+                              </div>  
+
+                              <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                           <div class="card" >
+                            <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                            <img class="activator" src={Image2} style={{width:'8em'}} />
+                            </div>
+                            
+                            <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                           
+                                 <center>
+                                 <h4 style={{width:'8em'}}>Wonder Women</h4>    
+                                 <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                 <br/>
+                                 <h3 style={{width:'auto',color:'red'}}> $74.99</h3>
+                                 </center>
+                             </div>
+                        </div>              
+                              </div>  
+
+                              <div className="col-3 col-md-3 col-sm-3"  style={{padding:'1em',cursor:'pointer'}}>
+                           
+                           <div class="card" >
+                            <div class="card-image waves-effect waves-block waves-light col-lg-1" style={{marginRight:'4em'}}>
+                            <img class="activator" src={Image} style={{width:'8em'}} />
+                            </div>
+                            
+                            <div class="card-reveal col-lg-3" style={{paddingTop:'1em'}}>
+                           
+                                 <center>
+                                 <h4 style={{width:'8em'}}>Bling Mate OG</h4>    
+                                 <h5 style={{width:'8em'}}>7.0 X 28</h5>
+                                 <br/>
+                                 <h3 style={{width:'auto',color:'red'}}> $84.99</h3>
+                                 </center>
+                             </div>
+                        </div>              
+                              </div>  
+                     
+                    </div>
+            </div>
+        );
+    }
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Logo from './image.svg';
+import Search from './search.png';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="col-lg-12 col-md-12 col-sm-12" style={{background:'white',borderBottom:'2.5px solid #F6510E'}}>
+                <div className="col-lg-10 col-md-10 col-sm-10" >
+                    <div className="col-lg-6 col-md-8 col-sm-12" > 
+                        <img src={Logo} style={{width:'auto',cursor:'pointer'}} alt="Logo"/>
+                    </div>
+                   
+                     <div className="col-lg-6 col-md-10 col-sm-12" style={{paddingTop:'2em'}}>
+                     <nav className="navbar navbar-inverse" style={{border:'none'}}>
+                    <div className="container-fluid" style={{backgroundColor:'white'}}>
+                        <div className="navbar-header" >
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style={{background:'black'}}>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>                        
+                        </button>
+                       
+                        </div>
+                        <div className="collapse navbar-collapse" id="myNavbar" style={{backgroundColor:'white'}}>
+                       
+                        <ul className="nav navbar-nav navbar-right">
+                            <li className="li"><a style={{color:'black',fontFamily:'Montserrat,sans-serif',fontSize:'1em'}} href="#"><img src={Search} style={{width:'auto'}}/>SEARCH</a></li>
+                            <li><a style={{color:'black',fontFamily:'Montserrat,sans-serif',fontSize:'1em'}} href="#">
+                          
+                            SUPPORT</a></li>
+                           
+                            <li><a style={{color:'black',fontFamily:'Montserrat,sans-serif',fontSize:'1em'}} href="#">GIFT CARD</a></li>       
+                            <li><a style={{color:'black',fontFamily:'Montserrat,sans-serif',fontSize:'1em'}} href="#">LOGIN</a></li>
+                          
+                        </ul>
+                        </div>
+                    </div>
+                    </nav>
+           </div>
+
+            </div>
+                
+
+            </div>
+        );
+    }
+}
+
+export default Header;
